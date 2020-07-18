9a672752d5f0454c1d4b303cc932f259532510a9
Mustafa Demir <mustafa.demir@kboxglobal.com>
2020-07-18 13:51:43 +0300

    vee-validate integration

--- todo-homework/src/components/todoForm.vue
+++ todo-homework/src/components/todoForm.vue
@@ -1,51 +1,67 @@
 <template>
     <div>
-        <form @submit.prevent="submitForm">
-        <div class="row mt-3">
-            <div class="col-12">
-                <label>Title</label>
-                <input type="text" class="w-100" v-model="title" />
-            </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col-12">
-                <label>Detail</label>
-                <input type="text" class="w-100" v-model="detail" />
-            </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col-12">
-                <label>Status</label>
-                <select class="w-100 px-1 py-1" v-model="status">
-                    <template v-for="status in Object.keys(STATUSSES)">
-                        <option :value="STATUSSES[status].VALUE">
-                            {{ STATUSSES[status].TEXT }}
-                        </option>
-                    </template>
-                </select>
-            </div>
-        </div>
-        <div class="row mt-4">
-            <div class="col-12">
-                <button type="submit" class="w-100 text-white"
-                :class="isUpdate ? 'bg-danger' : 'bg-success'">
-                    {{ isUpdate ? 'Update' : 'Add New' }}
-                </button>
-            </div>
-        </div>
-        </form>
+        <ValidationObserver v-slot="{ handleSubmit }">
+            <form @submit.prevent="handleSubmit(submitForm)">
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <label>Title</label>
+                        <ValidationProvider mode="passive" rules="required" v-slot="{ errors }">
+                            <input type="text" name="title" class="w-100" v-model="title" />
+                            <span class="text-danger">{{ errors[0] }}</span>
+                        </ValidationProvider>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <label>Detail</label>
+                        <ValidationProvider mode="passive" rules="required" v-slot="{ errors }">
+                            <input type="text" name="detail" class="w-100" v-model="detail" />
+                            <span class="text-danger">{{ errors[0] }}</span>
+                        </ValidationProvider>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <label>Status</label>
+                        <ValidationProvider mode="passive" rules="required" v-slot="{ errors }">
+                            <select class="w-100 px-1 py-1" name="status" v-model="status">
+                                <template v-for="status in Object.keys(STATUSSES)">
+                                    <option :value="STATUSSES[status].VALUE">
+                                        {{ STATUSSES[status].TEXT }}
+                                    </option>
+                                </template>
+                            </select>
+                            <span class="text-danger">{{ errors[0] }}</span>
+                        </ValidationProvider>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-12">
+                        <button type="submit" class="w-100 text-white"
+                        :class="isUpdate ? 'bg-danger' : 'bg-success'">
+                            {{ isUpdate ? 'Update' : 'Add New' }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </ValidationObserver>
     </div>
 </template>
 
 <script>
     import { STATUSSES } from '../statuses';
+    import { ValidationProvider, ValidationObserver } from 'vee-validate';
     export default {
         name: "todoForm",
+        components: {
+            ValidationProvider,
+            ValidationObserver,
+        },
         data() {
             return {
                 title: '',
                 detail: '',
-                status: 0,
+                status: '',
                 isUpdate: false,
                 STATUSSES
             }
--- todo-homework/src/main.js
+++ todo-homework/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import { extend, localize } from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+import tr from 'vee-validate/dist/locale/tr.json';
 
+// install rules and localization
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule]);
+});
+localize('tr', tr);
+
+
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 Vue.config.productionTip = false
 
 new Vue({
